93eae3ff2e9f1dddde6b8972cb0779db3e705b2c
benoitchampougny <fanatiqueberrurier@hotmail.com>
2016-02-19 17:47:15 +0100

    library

---
+++ 03-Library/raspberry/B+.py
@@ -0,0 +1,63 @@
+# -*-coding: utf-8 -*
+
+#####################################################################################################
+#                       TITLE
+#####################################################################################################
+
+brand="raspberry"
+model="b+"
+
+#####################################################################################################
+#                       ACCOUNT PIN
+#####################################################################################################
+
+number_of_pin_digital_GPIO = 40
+
+
+#####################################################################################################
+#                       DETAIL PIN
+#####################################################################################################
+
+
+detail_of_pin_digital_GPIO [1]= ["3.3v"]
+detail_of_pin_digital_GPIO [2]= ["5v"]
+detail_of_pin_digital_GPIO [3]= ["gpio","sda"]
+detail_of_pin_digital_GPIO [4]= ["5v"]
+detail_of_pin_digital_GPIO [5]= ["gpio","scl"]
+detail_of_pin_digital_GPIO [6]= ["ground"]
+detail_of_pin_digital_GPIO [7]= ["gpio"]
+detail_of_pin_digital_GPIO [8]= ["gpio","tx"]
+detail_of_pin_digital_GPIO [9]= ["ground","rx"]
+detail_of_pin_digital_GPIO [10]= ["gpio"]
+detail_of_pin_digital_GPIO [11]= ["gpio"]
+detail_of_pin_digital_GPIO [12]= ["gpio"]
+detail_of_pin_digital_GPIO [13]= ["gpio"]
+detail_of_pin_digital_GPIO [14]= ["ground"]
+detail_of_pin_digital_GPIO [15]= ["gpio"]
+detail_of_pin_digital_GPIO [16]= ["gpio"]
+detail_of_pin_digital_GPIO [17]= ["3.3v"]
+detail_of_pin_digital_GPIO [18]= ["gpio"]
+detail_of_pin_digital_GPIO [19]= ["gpio","spi_mosi"]
+detail_of_pin_digital_GPIO [20]= ["ground"]
+detail_of_pin_digital_GPIO [21]= ["gpio","spi_miso"]
+detail_of_pin_digital_GPIO [22]= ["gpio"]
+detail_of_pin_digital_GPIO [23]= ["gpio","spi_clk"]
+detail_of_pin_digital_GPIO [24]= ["gpio","spi_ce"]
+detail_of_pin_digital_GPIO [25]= ["ground","spi_ce"]
+detail_of_pin_digital_GPIO [26]= ["gpio"]
+detail_of_pin_digital_GPIO [27]= ["id_sd"]
+detail_of_pin_digital_GPIO [28]= ["id_sc"]
+detail_of_pin_digital_GPIO [29]= ["gpio"]
+detail_of_pin_digital_GPIO [30]= ["ground"]
+detail_of_pin_digital_GPIO [31]= ["gpio"]
+detail_of_pin_digital_GPIO [32]= ["gpio"]
+detail_of_pin_digital_GPIO [33]= ["gpio"]
+detail_of_pin_digital_GPIO [34]= ["ground"]
+detail_of_pin_digital_GPIO [35]= ["gpio"]
+detail_of_pin_digital_GPIO [36]= ["gpio"]
+detail_of_pin_digital_GPIO [37]= ["gpio"]
+detail_of_pin_digital_GPIO [38]= ["gpio"]
+detail_of_pin_digital_GPIO [39]= ["ground"]
+detail_of_pin_digital_GPIO [40]= ["gpio"]
+
+
